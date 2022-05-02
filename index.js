0bc1ef4579b42b2c96c81f5417a6a738e70939e7
sj06101996 <jaiswalshreya192@gmail.com>
2022-05-02 16:54:26 +0530

    basic formate adding

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("yes working")
+//used to log any information
+const winston = require("winston");
+const express = require("express");
+//used to log api calls
+var morgan = require('morgan');
+const app = express()
+
+
+app.use(morgan('combined'));
+
+const logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+
+
+
+
+
+app.get("/user",(req , res )=>{
+    return res.status(200).json({msg : "it is working"})
+})
+app.listen(3000,( err )=>{
+    logger.info("server is runiing on port number 3000")
+})
